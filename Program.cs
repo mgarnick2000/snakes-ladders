fec2eb42638e2f151576c904eb01cf370e40a8b5
Mark <mgarnick@techtonicgroup.com>
2019-09-26 16:22:28 -0600

    added logic to play snakes and ladders and added writelines

--- Program.cs
+++ Program.cs
@@ -1,86 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using static System.Console;
 
 namespace snakes_ladders
 {
     class Program
     {
-        private static Dictionary<int, int> snl = new Dictionary<int, int>() {
-            {4, 14},
-            {9, 31},
-            {17, 7},
-            {20, 38},
-            {28, 84},
-            {40, 59},
-            {51, 67},
-            {54, 34},
-            {62, 19},
-            {63, 81},
-            {64, 60},
-            {71, 91},
-            {87, 24},
-            {93, 73},
-            {95, 75},
-            {99, 78},
-        };
+        public static int[] playerSquare = new int[2] { 0, 0 };
+        public static int player = 0;
+        public static bool won = false;
+        public static int[,] trap = new int[21, 2]{{2,38},{7,14},{8,31},{15,26},{21,42},{28,84},{36,44},{51,67},{71,91},{78,98},{87,94},
+                    {16,6},{46,25},{49,11},{62,19},{64,60},{74,53},{89,68},{92,88},{95,75},{99,80}};
 
-        private static Random rand = new Random();
-        private const bool sixesThrowAgain = true;
-
-        static int Turn(int player, int square)
-        {
-            while (true)
-            {
-                int roll = rand.Next(1, 6);
-                Console.Write("Player {0}, on square {0}, rolls a {0}", player, square, roll);
-                if (square + roll > 100)
-                {
-                    Console.WriteLine(" but cannot move.");
-                }
-                else
-                {
-                    square += roll;
-                    Console.WriteLine(" and moves to square {0}", square);
-                    if (square == 100) return 100;
-                    int next = square;
-                    if (snl.ContainsKey(square))
-                    {
-                        next = snl[square];
-                    }
-                    if (square < next)
-                    {
-                        Console.WriteLine("Yay! Landed on a ladder. Climb up to {0}.", next);
-                        if (next == 100) return 100;
-                        square = next;
-                    }
-                    else if (square > next)
-                    {
-                        Console.WriteLine("Oops! Landed on a snake. Slither down to {0}.", next);
-                    }
-                }
-                if (roll < 6 || !sixesThrowAgain) return square;
-                Console.WriteLine("Rolled a 6 so roll again.");
-            }
-        }
         static void Main(string[] args)
         {
-            int[] players = { 1, 1, 1 };
-            while (true)
-            {
-                for (int i = 0; i < players.Length; i++)
-                {
-                    int ns = Turn(i + 1, players[i]);
-                    if (ns == 100)
-                    {
-                        Console.WriteLine("Player {0} wins!", i + 1);
-                        return;
-                    }
-                    players[i] = ns;
-                    Console.WriteLine();
-                }
-            }
+           
+           play(6,6);
+           play(4, 3);
+            
         }
 
+        public static string play(int die1, int die2)
+        {
+            if (won) { WriteLine("Game Over"); return "Game over!"; }
+            int roll = die1 + die2;
+            if (roll + playerSquare[player] <= 100)
+            {
+                playerSquare[player] += roll;
+                if (playerSquare[player] == 100) { won = true; WriteLine("Player " + (player + 1).ToString() + " Wins!"); return "Player " + (player + 1).ToString() + " Wins!"; }
+            }
+            else
+            {
+                playerSquare[player] = 100 - ((playerSquare[player] + roll) - 100);
+            }
+            for (int t = 0; t < trap.Length / 2; t++)
+            {
+                if (playerSquare[player] == trap[t, 0]) { playerSquare[player] = trap[t, 1]; }
+            }
+            string message = "Player " + (player + 1).ToString() + " is on square " + (playerSquare[player]).ToString();
+            if (die1 != die2) { if (player == 0) { player = 1; } else { player = 0; }; }
+            WriteLine(message);
+            return message;
+        }
 
     }
 }
